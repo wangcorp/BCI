2cf0834a0f7569629a8d1627c5cb662d253d35dc
Ruijia <w.ruijia@gmail.com>
2018-06-05 12:04:16 +0200

    updated alpha in patterndecoder

--- PatternRecognition.m
+++ PatternRecognition.m
@@ -2,28 +2,30 @@
 close all;
 %clear all;
 
-% %RUIJIA - laptop
+tic;
+
+% %RUIJIA - cartop
 % addpath(genpath('C:\Users\Ruijia\Documents\EPFL\BCI\Project - Commmon material\biosig')); %to add the right library
 % addpath(genpath('C:\Users\Ruijia\Documents\EPFL\BCI\Project - Commmon material\eeglab13_4_4b'));
-% load('C:\Users\Ruijia\Documents\EPFL\BCI\Project - Commmon material\laplacian_16_10-20_mi.mat');
+% load('C:\Users\Ruijia\Documents\EPFL\BCI\Project - Commmon material\carlacian_16_10-20_mi.mat');
 % load('C:\Users\Ruijia\Documents\EPFL\BCI\Project - Commmon material\channel_location_16_10-20_mi.mat');
 
 % RUIJIA - home
-% addpath(genpath('C:\Users\Waz\Documents\EPFL\BCI\Common\biosig'));
-% addpath(genpath('C:\Users\Waz\Documents\EPFL\BCI\Common\eeglab13_4_4b'));
-% load('C:\Users\Waz\Documents\EPFL\BCI\Common\laplacian_16_10-20_mi.mat');
-% load('C:\Users\Waz\Documents\EPFL\BCI\Common\channel_location_16_10-20_mi.mat');
+addpath(genpath('C:\Users\Waz\Documents\EPFL\BCI\Common\biosig'));
+addpath(genpath('C:\Users\Waz\Documents\EPFL\BCI\Common\eeglab13_4_4b'));
+load('C:\Users\Waz\Documents\EPFL\BCI\Common\laplacian_16_10-20_mi.mat');
+load('C:\Users\Waz\Documents\EPFL\BCI\Common\channel_location_16_10-20_mi.mat');
 
-%SEB
-addpath(genpath('/Users/sebh/Dropbox/EPFL2/EPFL/1MASTER/Semestre2/BCI/Project/ai6_micontinuous/20180316/biosig'));
-addpath(genpath('/Users/sebh/Dropbox/EPFL2/EPFL/1MASTER/Semestre2/BCI/Projects - Common material-20180301/eeglab13_4_4b'));
-load('/Users/sebh/Dropbox/EPFL2/EPFL/1MASTER/Semestre2/BCI/Project/ai6_micontinuous/20180316/laplacian_16_10-20_mi.mat');
-load('/Users/sebh/Dropbox/EPFL2/EPFL/1MASTER/Semestre2/BCI/Project/ai6_micontinuous/20180316/channel_location_16_10-20_mi.mat');
+% %SEB
+% addpath(genpath('/Users/sebh/Dropbox/EPFL2/EPFL/1MASTER/Semestre2/BCI/Project/ai6_micontinuous/20180316/biosig'));
+% addpath(genpath('/Users/sebh/Dropbox/EPFL2/EPFL/1MASTER/Semestre2/BCI/Projects - Common material-20180301/eeglab13_4_4b'));
+% load('/Users/sebh/Dropbox/EPFL2/EPFL/1MASTER/Semestre2/BCI/Project/ai6_micontinuous/20180316/carlacian_16_10-20_mi.mat');
+% load('/Users/sebh/Dropbox/EPFL2/EPFL/1MASTER/Semestre2/BCI/Project/ai6_micontinuous/20180316/channel_location_16_10-20_mi.mat');
 
 % % Emilie
 % addpath(genpath('/Users/emilierevol/Desktop/BCI/Project 2 - Naturally controlling a MI BCI-driven robot/project2-data-example/biosig'));
 % addpath(genpath('/Users/emilierevol/Desktop/BCI/Project 2 - Naturally controlling a MI BCI-driven robot/project2-data-example/eeglab13_4_4b'));
-% load('/Users/emilierevol/Desktop/BCI/Projects - Common material/laplacian_16_10-20_mi.mat');
+% load('/Users/emilierevol/Desktop/BCI/Projects - Common material/carlacian_16_10-20_mi.mat');
 % load('/Users/emilierevol/Desktop/BCI/Projects - Common material/channel_location_16_10-20_mi.mat');
 
 %% INFO
@@ -35,39 +37,51 @@ load('/Users/sebh/Dropbox/EPFL2/EPFL/1MASTER/Semestre2/BCI/Project/ai6_micontinu
 load(filename);
 
 %% Best Features selection and training data formating
-sel_freq = [8,9,10]; %to fill according to the selected best features (frequencies)
-sel_chan = [9,9,9]; %to fill according to the selected best features (channels)
+% Seb
+% sel_freq = [8,9,10]; %to fill according to the selected best features (frequencies)
+% sel_chan = [9,9,9]; %to fill according to the selected best features (channels)
+
+% Ruijia
+% sel_freq = [6,6,11]; %to fill according to the selected best features (frequencies)
+% sel_chan = [7,11,9]; %to fill according to the selected best features (channels)
+
+% Emilie
+sel_freq = [6,6,6,6]; %to fill according to the selected best features (frequencies)
+sel_chan = [11,16,7,12]; %to fill according to the selected best features (channels)
+
+
 
 for i=1:length(sel_chan)
     if i==1
-        PSD_feat_lap=PSD_lap((labels_lap==771 | labels_lap==773), sel_freq(1), sel_chan(1));
+        PSD_feat_car=PSD_car((labels_car==771 | labels_car==773), sel_freq(1), sel_chan(1));
     end
-    PSD_feat_lap=[PSD_feat_lap,  PSD_lap((labels_lap==771 | labels_lap==773), sel_freq(i), sel_chan(i))];
+    PSD_feat_car=[PSD_feat_car,  PSD_car((labels_car==771 | labels_car==773), sel_freq(i), sel_chan(i))];
 end
-PSD_feat_lap = log(PSD_feat_lap);
-labels_feat = labels_lap(labels_lap==771 | labels_lap==773);
+PSD_feat_car = log(PSD_feat_car);
+labels_feat = labels_car(labels_car==771 | labels_car==773);
 
 %% Training
-classifier = fitcdiscr(PSD_feat_lap, labels_feat);
-%[yhat, score] = predict(classifier, PSD_feat_lap);
+classifier = fitcdiscr(PSD_feat_car, labels_feat);
+%[yhat, score] = predict(classifier, PSD_feat_car);
 
 %% Test data formating
 % Using features selected from training data
 DataPreprocessing;
 for i=1:length(sel_chan)
     if i==1
-        PSD_feat_lap_online = PSD_lap(:, sel_freq(1), sel_chan(1));
+        PSD_feat_car_online = PSD_car(:, sel_freq(1), sel_chan(1));
     end
-    PSD_feat_lap_online=[PSD_feat_lap_online,  PSD_lap(:, sel_freq(i), sel_chan(i))];
+    PSD_feat_car_online=[PSD_feat_car_online,  PSD_car(:, sel_freq(i), sel_chan(i))];
 end
-PSD_feat_lap_online = log(PSD_feat_lap_online);
+PSD_feat_car_online = log(PSD_feat_car_online);
 
 %% Classifier (testing)
 %Tweakable variables
-alpha=0.05;
+alpha=0.89;
 trial_continuous = true;
 y_down = 0.3;
 y_up = 0.7;
+
 %ROC VARIABLES:
 FPR = [];
 TPR = [];
@@ -76,13 +90,13 @@ sco_on = [];
 
 dt_total = [];
 
-event_trial = find(event_lap(:,1)==771 | event_lap(:,1)==773);
+event_trial = find(event_car(:,1)==771 | event_car(:,1)==773);
 decision = zeros(1,length(event_trial));
 decision_time = zeros(1,length(event_trial));
 
 decision_true = ones(1,length(event_trial));
 for i=1:length(event_trial)
-    if event_lap(event_trial(i),1)==771
+    if event_car(event_trial(i),1)==771
         decision_true(i) = -1;
     end
 end
@@ -92,10 +106,10 @@ switch trial_continuous
         dt_memory = {};
         for trialID = 1:length(event_trial)
             dt=0.5;
-            for windowID = 2:event_lap(event_trial(trialID),3)+event_lap(event_trial(trialID)+1,3)
-                [yhat_online,score_online] = predict(classifier,PSD_feat_lap_online(event_lap(event_trial(trialID),2)+windowID-1,:));
+            for windowID = 2:event_car(event_trial(trialID),3)+event_car(event_trial(trialID)+1,3)
+                [yhat_online,score_online] = predict(classifier,PSD_feat_car_online(event_car(event_trial(trialID),2)+windowID-1,:));
                 ppt = score_online(:,2); % Column 1: feet (773), column 2: hands (771)
-                dt=[dt,ppt*alpha + dt(:,windowID-1)*(1-alpha)];
+                dt=[dt,ppt*(1-alpha) + dt(:,windowID-1)*alpha];
                 sco_on = [sco_on; score_online]; %For the ROC
 
                 if dt(windowID) >= y_up
@@ -110,7 +124,7 @@ switch trial_continuous
         end
         
     case true
-        dt_memory = zeros(1,length(PSD_feat_lap_online)-1);
+        dt_memory = zeros(1,length(PSD_feat_car_online)-1);
         dt_memory = [0.5, dt_memory];
         
         trial_counter = 0;
@@ -119,10 +133,10 @@ switch trial_continuous
         current_trial = 2;
         dt_old = 0.5;
         
-        for windowID = 2:length(PSD_feat_lap_online)
-            [yhat,score_online]=predict(classifier,PSD_feat_lap_online(windowID,:));
+        for windowID = 2:length(PSD_feat_car_online)
+            [yhat,score_online]=predict(classifier,PSD_feat_car_online(windowID,:));
             ppt = score_online(:,2);
-            trial_start = (windowID == event_lap(event_trial,2));
+            trial_start = (windowID == event_car(event_trial,2));
             trial_start_sort = sort(trial_start);
             
             sco_on = [sco_on; score_online]; %For the ROC
@@ -135,11 +149,11 @@ switch trial_continuous
                 dt=0.5;
                 dt_old = dt;
             else
-                dt=ppt*alpha + dt_old*(1-alpha);
+                dt=ppt*(1-alpha) + dt_old*alpha;
                 dt_old = dt;
             end
             
-            if windowID == event_lap(current_trial,2)+event_lap(current_trial,3)+event_lap(current_trial+1,3)
+            if windowID == event_car(current_trial,2)+event_car(current_trial,3)+event_car(current_trial+1,3)
                 trial_check=false;
             end
             
@@ -147,7 +161,7 @@ switch trial_continuous
                 trial_time = trial_time+1;
                 if dt >= y_up
                     decision(trial_counter)=1; % hands
-                    decision_time(trial_counter)=trial_time;%-event_lap(event_trial(current_trial),2);
+                    decision_time(trial_counter)=trial_time;%-event_car(event_trial(current_trial),2);
                     trial_check = false;
                 elseif dt <= y_down
                     decision(trial_counter)=-1; % feet
@@ -162,8 +176,8 @@ switch trial_continuous
 end
 
 %% ROC VALUES and plotting:
-labels_lap = labels_lap(2:end); %Starting from window 2                                    
-[FPR, TPR, ~, AUC] = perfcurve(labels_lap(labels_lap==771 | labels_lap==773), sco_on((labels_lap==771 | labels_lap==773),2), 773);
+labels_car = labels_car(2:end); %Starting from window 2                                    
+[FPR, TPR, ~, AUC] = perfcurve(labels_car(labels_car==771 | labels_car==773), sco_on((labels_car==771 | labels_car==773),2), 773);
 x = linspace(0,1,10);
 y = linspace(0,1,10);
 plot(FPR,TPR,'-r')
@@ -176,31 +190,31 @@ title('ROC curve')
 text(0.6,0.2,te);
 
 %% Plot Task classifier
-figure;
-hold on
-for i=1:length(event_trial)
-    if event_lap(event_trial(i),1) == 771
-        rectangle('Position', [event_lap(event_trial(i),2) 0 event_lap(event_trial(i),3)+event_lap(event_trial(i)+1,3) 1], 'FaceColor', [0 0 1 0.2],'LineStyle','none');
-    else
-        rectangle('Position', [event_lap(event_trial(i),2) 0 event_lap(event_trial(i),3)+event_lap(event_trial(i)+1,3) 1], 'FaceColor', [1 0 0 0.2],'LineStyle','none');
-    end
-end
-
 if trial_continuous == true
+    figure;
+    hold on
+    for i=1:length(event_trial)
+        if event_car(event_trial(i),1) == 771
+            rectangle('Position', [event_car(event_trial(i),2) 0 event_car(event_trial(i),3)+event_car(event_trial(i)+1,3) 1], 'FaceColor', [0 0 1 0.2],'LineStyle','none');
+        else
+            rectangle('Position', [event_car(event_trial(i),2) 0 event_car(event_trial(i),3)+event_car(event_trial(i)+1,3) 1], 'FaceColor', [1 0 0 0.2],'LineStyle','none');
+        end
+    end
+    
     plot(dt_memory, 'Color', 'k');
-    line([0,length(PSD_feat_lap_online)],[y_down,y_down], 'Color', 'k');
-    line([0,length(PSD_feat_lap_online)],[y_up,y_up], 'Color', 'k');
-    % xlim([2000 3200]);
-    title('Posterior probability as a function of time window and task');
+    line([0,length(PSD_feat_car_online)],[y_down,y_down], 'Color', 'k');
+    line([0,length(PSD_feat_car_online)],[y_up,y_up], 'Color', 'k');
+    xlim([0 5000]);
+    title('Decision probability as a function of time window and task');
     xlabel('time window [-]');
-    ylabel('posterior probability [-]');
+    ylabel('Decision probability [-]');
     hold off
 end
 
 %% Classifier performance evaluation
 %time estimation
-decision_time_mean = mean(decision_time(decision_time~=0));
-
+decision_time_mean = (mean(decision_time(decision_time~=0))*32)/512;
+decision_time_std = (std(decision_time)*32)/512;
 %error estimation
 miss_class = 0;
 for i=1:length(decision_true)
@@ -242,3 +256,5 @@ support.lap = lap;
 support.feat = [sel_freq;sel_chan];
 support.car = false;
 save('support.mat', 'support');
+
+toc;
